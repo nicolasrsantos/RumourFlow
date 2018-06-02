78a1bbfe69ae8904d678e1a1cf0b6f0fcd90aaf1
Nicolas Roque <nicolas.rsantos1@gmail.com>
2018-06-02 20:22:22 -0300

    data processing code

--- TwitterAPI/src/dataUtil/dataUtil.java
+++ TwitterAPI/src/dataUtil/dataUtil.java
@@ -4,12 +4,20 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+import org.apache.commons.io.FileUtils;
+
+import com.github.jreddit.memes.MemesJSON;
+import com.github.jreddit.memes.Nodes;
+import com.google.gson.Gson;
+
+import Utils.PropertyUtils;
 import sentiment.SentimentString;
 
 public class dataUtil
@@ -42,13 +50,42 @@ public class dataUtil
 		}
 	}
 	
-	public static void main(String[] args)
+	public static String getRootMessage(String user, List<Nodes> nodes)
 	{
-		try {
-			extractSentiment("/home/nicolas/Downloads/twitter_gt.csv", "/home/nicolas/Downloads/test.csv");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		for (Nodes node : nodes)
+			if (node.getLabel().equals(user))
+				return node.getTitle();
+		
+		return null;
+	}
+	
+	public static void dataToCsv(String inputDir, String keyword) throws IOException
+	{
+		String json = FileUtils.readFileToString(new File(inputDir + keyword + ".json"));
+		
+		Gson gson = new Gson();
+		MemesJSON posts = gson.fromJson(json, MemesJSON.class);
+		List<Nodes> nodes = posts.getNodes();
+		List<Nodes> userNodes = posts.getUserNodes();
+		
+		PrintWriter fileWriter = new PrintWriter(inputDir + keyword + ".csv");
+		
+		for (Nodes node : userNodes)
+		{
+			if (node.getLabel().equals(node.getTitle()))
+				fileWriter.println(getRootMessage(node.getLabel(), nodes));
+			else
+				fileWriter.println(node.getLabel());
 		}
+		
+		fileWriter.close();
+	}
+	
+	public static void main(String[] args) throws IOException
+	{
+		String keyword = "trump biggest tax cut_reddit";
+		String inputDir = new StringBuilder().append(PropertyUtils.getCommentPath()).toString();
+		
+		dataToCsv(inputDir, keyword);
 	}
 }
